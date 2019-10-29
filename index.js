a363c770a13d4610438aae1f15f19d8f65c55d97
Jason Sonnichsen <Harliquinn@gmail.com>
2019-10-29 19:11:13 -0400

    Server set up from example

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code away!
+const express = require("express");
+const PostDb = require("./posts/postDb");
+const UserDb = require("./users/userDb");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
+
+const port = 5000;
+const server = express();
+server.use(express.json());
+server.use("/user", userRouter);
+server.use("/post", postRouter);
+
+const errorHelper = (status, message, res) => {
+  res.status(status).json({ error: message });
+};
+
+const nameCheckMiddleware = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    errorHelper(404, "Name must be included", res);
+    next();
+  } else {
+    next();
+  }
+};
+
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,112 @@
-const express = 'express';
-
+const express = require("express");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post("/", (req, res) => {
+  const { name } = req.body;
+  users
+    .insert({ name })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      return errorHelper(500, "Database boof", res);
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {});
 
+router.get("/", (req, res) => {
+  users
+    .get()
+    .then(foundUsers => {
+      res.json(foundUsers);
+    })
+    .catch(err => {
+      return errorHelper(500, "Database boof", res);
+    });
 });
 
-router.get('/', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  users
+    .get(id)
+    .then(user => {
+      if (user === 0) {
+        return errorHelper(404, "No user by that Id in the DB", res);
+      }
+      res.json(user);
+    })
+    .catch(err => {
+      return errorHelper(500, "Database boof", res);
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:userId/posts", (req, res) => {
+  const { userId } = req.params;
+  users
+    .getUserPosts(userId)
+    .then(usersPosts => {
+      if (usersPosts === 0) {
+        return errorHelper(404, "No posts by that user", res);
+      }
+      res.json(usersPosts);
+    })
+    .catch(err => {
+      return errorHelper(500, "Database boof", res);
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users
+    .remove(id)
+    .then(userRemoved => {
+      if (userRemoved === 0) {
+        return errorHelper(404, "No user by that id");
+      } else {
+        res.json({ success: "User Removed" });
+      }
+    })
+    .catch(err => {
+      return errorHelper(500, "Database boof", res);
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  users
+    .update(id, { name })
+    .then(response => {
+      if (response === 0) {
+        return errorHelper(404, "No user by that id");
+      } else {
+        db.find(id).then(user => {
+          res.json(user);
+        });
+      }
+    })
+    .catch(err => {
+      return errorHelper(500, "Database boof", res);
+    });
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
+function validatePost(req, res, next) {}
 
-};
-
-function validatePost(req, res, next) {
-
+const nameCheckMiddleware = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    errorHelper(404, "Name must be included", res);
+    next();
+  } else {
+    next();
+  }
 };
 
 module.exports = router;
